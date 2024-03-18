b2859af74fb64067491b87655c47ffe9b4c8928a
ichbinrich <r.martinez.ramirez@tu-berlin.de>
2024-03-18 17:35:45 +0100

    #26 precommit pass

--- tests/questions/test_question_parsing.py
+++ tests/questions/test_question_parsing.py
@@ -1,21 +1,21 @@
 import sys
 from pathlib import Path
 from textwrap import dedent
-import yaml
 
 import pytest
+import yaml
 
 from moodle_tools.make_questions import main
+from moodle_tools.questions import TrueFalseQuestion, converter
 from moodle_tools.questions.factory import QuestionFactory
-from moodle_tools.questions import converter
-from moodle_tools.questions import TrueFalseQuestion
+
 
 class TestGeneralQuestion:
     def test_question_type_property(self):
 
-        #Input from a true_false question type
+        # Input from a true_false question type
         input_yaml_with_property = dedent(
-        """
+            """
         ---
         type: true_false
         statement: "Minimal false question"
@@ -24,7 +24,7 @@ class TestGeneralQuestion:
         )
 
         input_yaml_with_no_property = dedent(
-        """
+            """
         ---
         statement: "Minimal false question"
         correct_answer: false
@@ -32,25 +32,25 @@ class TestGeneralQuestion:
         )
 
         question = converter.load_questions(
-            QuestionFactory, 
-            False, 
-            yaml.safe_load_all(input_yaml_with_property), 
-            markdown=False, 
+            QuestionFactory,
+            False,
+            yaml.safe_load_all(input_yaml_with_property),
+            markdown=False,
             table_border=False,
             title="Knowledge question",
-            )
+        )
         question_with_type = next(question)
 
         question = converter.load_questions(
-            QuestionFactory, 
-            False, 
-            yaml.safe_load_all(input_yaml_with_no_property), 
-            markdown=False, 
+            QuestionFactory,
+            False,
+            yaml.safe_load_all(input_yaml_with_no_property),
+            markdown=False,
             table_border=False,
             title="Knowledge question",
-            )
+        )
         question_with_no_type = next(question)
 
         # Assert the output is as expected
-        assert isinstance(question_with_type,TrueFalseQuestion)
+        assert isinstance(question_with_type, TrueFalseQuestion)
         assert question_with_no_type == "Question type not supported."
