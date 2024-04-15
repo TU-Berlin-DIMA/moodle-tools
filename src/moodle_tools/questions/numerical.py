fa4fccaac4f9e780ad64e304776821dd9cce8e04
ichbinrich <r.martinez.ramirez@tu-berlin.de>
2024-04-15 18:48:21 +0200

    #11 Addressed check on at least one answer with 100 points and 1st answer correct assumption for numerical and sub-types.

--- src/moodle_tools/questions/numerical.py
+++ src/moodle_tools/questions/numerical.py
@@ -27,12 +27,22 @@ class NumericalQuestion(Question):
         ]
 
         # Update missing answer points and feedback
+        # TODO: Create corner case test fr this functionality
+        num_full_points: int = len(list(filter(lambda x: x["points"] == 100, self.answers)))
         for i, answer in enumerate(self.answers):
             if "points" not in answer:
-                answer["points"] = 100 if i == 0 else 0
+                if i == 0 and num_full_points == 0:
+                    answer["points"] = 100
+                    # TODO: Change this print to a LOG
+                    print("First answer is assumed to be correct.")
+                else:
+                    answer["points"] = 0
 
     def validate(self) -> list[str]:
         errors = super().validate()
+        num_full_points: int = len(list(filter(lambda x: x["points"] == 100, self.answers)))
+        if num_full_points == 0:
+            errors.append("At least one answer must have 100 points.")
         for answer in self.answers:
             if "feedback" not in answer and answer["points"] != 100:
                 errors.append(f"The incorrect answer '{answer['answer']}' has no feedback.")
