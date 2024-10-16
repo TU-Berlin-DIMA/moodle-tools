5d6a5e6f3d55681b5f8f659e08be2613f06173d9
ichbinrich <r.martinez.ramirez@tu-berlin.de>
2024-10-16 20:21:10 +0200

    #62 introduce xml parser, creates question from basic properties.

--- src/moodle_tools/extract_questions.py
+++ src/moodle_tools/extract_questions.py
@@ -2,12 +2,41 @@
 
 import argparse
 import sys
+import xml.etree.ElementTree as ET
 from pathlib import Path
 from typing import Iterator
 
 from loguru import logger
 
+from moodle_tools.questions import Question, QuestionFactory
 from moodle_tools.utils import iterate_inputs
+
+
+def load_moodle_xml(path: Path) -> Iterator[Question]:
+    with open(path) as file:
+        document = ET.parse(file)
+        quiz = document.getroot()
+
+    # Only one category allowed
+    category = ""
+    for element in quiz.findall("question"):
+        question_props = dict()
+        if element.attrib.get("type") == "category":
+            category = element.find("category").find("text").text
+        else:
+            question_props.update({"category": category})
+            question_props.update({"type": element.attrib.get("type")})
+            question_props.update({"title": element.find("name").find("text").text})
+            question_props.update({"question": element.find("questiontext").find("text").text})
+            question_props.update(
+                {"general_feedback": element.find("generalfeedback").find("text").text}
+            )
+            question_props.update({"markdown": False})
+            question_props.update({"table_styling": False})
+            question = QuestionFactory.create_question(
+                question_props.get("type"), **question_props
+            )
+            yield question
 
 
 def extract_yaml_questions(paths: Iterator[Path]) -> str:
@@ -19,13 +48,20 @@ def extract_yaml_questions(paths: Iterator[Path]) -> str:
     Returns:
         str: Moodle-Tools YAML for all questions in the XML file.
     """
+    questions: list[Question] = []
     for path in paths:
-        print(path)
-    return "TODO: Implement function"
+        for question in load_moodle_xml(path):
+            questions.append(question)
+            print(question.title)
+
+    logger.info(f"Loaded {len(questions)} questions from YAML.")
+    return "yaml"
 
 
 def parse_args() -> argparse.Namespace:
     # pylint: disable=duplicate-code
+    # TODO: refactor to comply with pylint and mypy.
+    # Marked as duplicated with make_question:parse_args, main
     """Parse command line arguments.
 
     Returns:
