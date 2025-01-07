d2c94b32bbdd2ab3d87c34fe5a160ff83b799bae
Gereon Dusella <gereon.dusella@tu-berlin.de>
2025-01-07 17:15:45 +0100

    fixed to_yaml by adding tree-based categories

--- src/moodle_tools/extract_questions.py
+++ src/moodle_tools/extract_questions.py
@@ -4,12 +4,15 @@ import argparse
 import sys
 import xml.etree.ElementTree as ET
 from base64 import b64decode
+from functools import reduce
+from operator import ior
 from pathlib import Path
-from typing import Any, Iterator
+from typing import Any, Callable, Iterator
 
 import pathvalidate
 import yaml
 from loguru import logger
+from yaml import Dumper, ScalarNode
 
 from moodle_tools.enums import ShuffleAnswersEnum
 from moodle_tools.questions import QuestionFactory
@@ -42,13 +45,38 @@ def load_moodle_xml(
             )  # fix for slashes in category names
         else:
             question_props.update({"category": category})
-            question_type = element.attrib.get("type")
-            question_props.update({"type": question_type})
-            question_props.update({"table_styling": table_styling})
+            question_type = str(element.attrib.get("type"))
+            question_props.update({"type": str(question_type)})
+            question_props.update({"table_styling": str(table_styling)})
 
             question = QuestionFactory.props_from_xml(question_type, element, **question_props)
 
             yield dict(sorted(question.items(), key=lambda item: ITEM_ORDER.get(item[0], 10)))
+
+
+def write_sidecar_files(quest: dict[str | Any | None, str], out_dir: Path) -> None:
+    if out_dir:
+        for name, file in quest.get("files", {}).items():
+            if file["is_used"]:
+                match file["encoding"]:
+                    case "base64":
+                        img_dir = out_dir / quest["category"]
+                        img_dir.mkdir(parents=True, exist_ok=True)
+                        filename = img_dir / name
+
+                        content = b64decode(file["content"])
+
+                        if filename.exists() and filename.read_bytes() != content:
+                            filename = filename.with_suffix(filename.suffix + "." + quest["title"])
+                            logger.warning(
+                                f"File {filename} already exists, saving as {filename.name} ."
+                            )
+                        with filename.open("wb") as f:
+                            f.write(content)
+    else:
+        logger.warning("No output path provided, additional files will not be saved.")
+
+        del quest["files"]
 
 
 def extract_yaml_questions(
@@ -74,70 +102,96 @@ def extract_yaml_questions(
 
     logger.info(f"Loaded {len(questions)} questions from YAML.")
 
-    category_parts = [
-        list(r) for r in zip(*[q["category"].split("/") for q in questions], strict=True)
-    ]
-    category_diff = [len(set(parts)) != 1 for parts in category_parts]
-
-    min_category_diff = min(p for p in [i * int(c) for i, c in enumerate(category_diff)] if p > 0)
-
-    grouped_questions: dict[str, list] = {}
     for question in questions:
-        category = pathvalidate.sanitize_filename(
-            "/".join(question["category"].split("/")[min_category_diff:]), platform="universal"
+        question["category"] = question["category"].replace("//", "::")
+
+    def build_cat_tree(children: list[dict[str, Any]], last_cat_level: str) -> dict[str, Any]:
+        curr_cat_questions = [c["question"] for c in children if len(c["cat_parts"]) == 0]
+
+        new_children = {c["cat_parts"][0]: [] for c in children if len(c["cat_parts"]) > 0}
+
+        if len(new_children) == 0:
+            return {"children": {}, "cat_name": last_cat_level, "questions": curr_cat_questions}
+
+        for c in children:
+            if len(c["cat_parts"]) > 0:
+                cat_name = c["cat_parts"].pop(0)
+                new_children[cat_name].append(c)
+
+        return {
+            "children": {k: build_cat_tree(v, k) for k, v in new_children.items()},
+            "cat_name": last_cat_level,
+            "questions": curr_cat_questions,
+        }
+
+    cat_root = build_cat_tree(
+        [{"cat_parts": q["category"].split("/"), "question": q} for q in questions], "root"
+    )
+
+    def remove_1_child_levels(cat_node: dict[str, Any] | None) -> dict[str, Any] | None:
+        if cat_node is None:
+            return None
+        if len(cat_node["children"]) == 1:
+            return remove_1_child_levels(list(cat_node["children"].values())[0])
+
+        return cat_node
+
+    cat_root = remove_1_child_levels(cat_root)
+
+    def build_paths(
+        cat_node: dict[str, Any] | None, path: str
+    ) -> dict[str, list[dict[str, str | Any | None]]]:
+        if cat_node is None:
+            return {}
+
+        path = path + "/" + cat_node["cat_name"] if path else cat_node["cat_name"]
+        curr_quest = {}
+        if cat_node["questions"]:
+            new_cat = pathvalidate.sanitize_filepath(path, platform="universal")
+            new_cat = new_cat if not cat_node["children"] else new_cat + "/in_category"
+            for q in cat_node["questions"]:
+                q["category"] = new_cat
+
+            curr_quest = {new_cat: cat_node["questions"]}
+
+        if len(cat_node["children"]) == 0:
+            return curr_quest
+
+        return curr_quest | reduce(
+            ior, [build_paths(c, path) for c in cat_node["children"].values()]
         )
-        if category not in grouped_questions:
-            grouped_questions[category] = []
-        grouped_questions[category].append(question)
 
-        if out_dir:
-            for name, file in question.get("files", {}).items():
-                if file["is_used"]:
-                    match file["encoding"]:
-                        case "base64":
-                            img_dir = out_dir / category
-                            img_dir.mkdir(parents=True, exist_ok=True)
-                            filename = img_dir / name
+    paths = build_paths(cat_root, "")
 
-                            content = b64decode(file["content"])
-
-                            if filename.exists() and filename.read_bytes() != content:
-                                filename = filename.with_suffix(
-                                    filename.suffix + "." + question["title"]
-                                )
-                                logger.warning(
-                                    f"File {filename} already exists, saving as {filename.name} ."
-                                )
-                            with filename.open("wb") as f:
-                                f.write(content)
-        else:
-            logger.warning("No output path provided, additional files will not be saved.")
-
-        del question["files"]
+    for quests in paths.values():
+        for question in quests:
+            write_sidecar_files(question, out_dir)
 
     class IndentDumper(yaml.SafeDumper):
         def increase_indent(self, flow=False, indentless=False) -> None:
             super().increase_indent(flow, False)
 
-    def str_presenter(dumper, data) -> Any:
+    def str_presenter(dumper: Callable[[Dumper, str], ScalarNode], data: Any) -> Any:
         if len(data.splitlines()) > 1:  # check for multiline string
             data = "\n".join([line.rstrip() for line in data.strip().splitlines()])
             return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
         return dumper.represent_scalar("tag:yaml.org,2002:str", data)
 
-    def sae_representer(dumper, data) -> Any:
+    def safe_representer(
+        dumper: Callable[[Dumper, ShuffleAnswersEnum], ScalarNode], data: Any
+    ) -> Any:
         return dumper.represent_scalar("tag:yaml.org,2002:str", data.upper())
 
     yaml.add_representer(str, str_presenter)
-    yaml.add_representer(ShuffleAnswersEnum, sae_representer)
+    yaml.add_representer(ShuffleAnswersEnum, safe_representer)
     yaml.representer.SafeRepresenter.add_representer(str, str_presenter)  # to use with safe_dum
-    yaml.representer.SafeRepresenter.add_representer(ShuffleAnswersEnum, sae_representer)
+    yaml.representer.SafeRepresenter.add_representer(ShuffleAnswersEnum, safe_representer)
     IndentDumper.add_representer(str, str_presenter)
-    IndentDumper.add_representer(ShuffleAnswersEnum, sae_representer)
+    IndentDumper.add_representer(ShuffleAnswersEnum, safe_representer)
 
     questions_yaml_grouped = {
         k: yaml.dump_all(q, width=200, sort_keys=False, allow_unicode=True, Dumper=IndentDumper)
-        for k, q in grouped_questions.items()
+        for k, q in paths.items()
     }
 
     return questions_yaml_grouped
@@ -224,7 +278,7 @@ def main() -> None:
 
     for category, moodle_tools_yaml in moodle_tools_yaml_grouped.items():
         if output_dir:
-            output_path = output_dir / category / f"questions_{category}.yaml"
+            output_path = output_dir / category / f"questions_{category.split('/')[-1]}.yaml"
             output_path.parent.mkdir(parents=True, exist_ok=True)
         print(
             moodle_tools_yaml,
