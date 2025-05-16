ad4c7dc5af83560c90bd289a2108882b5edbe92d
Lennart Behme <lennart.behme@tu-berlin.de>
2025-05-16 11:17:24 +0200

    Move to static version in pyproject.toml

--- src/moodle_tools/__init__.py
+++ src/moodle_tools/__init__.py
@@ -1,7 +1,6 @@
 """.. include:: ../../README.md"""
 
 __all__ = ["ParsingError", "analyze_results", "make_questions", "questions"]
-__version__ = "1.1.0+dev"
 
 from moodle_tools import analyze_results, make_questions, questions
 from moodle_tools.utils import ParsingError
